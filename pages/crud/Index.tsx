36d3a7810f42dda17ccef6ca514ce47a1d7feb45
Ryan Abi Nugraha <82674577+ryanabi-RA@users.noreply.github.com>
2023-02-17 15:47:28 +0000

    add new folder crud

---
+++ pages/crud/Index.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+type Props = {}
+
+export default function Index({}: Props) {
+  return (
+    <>
+    <div className="w-full h-screen border border-red-500">
+        <h1>hello</h1>
+    </div>
+    </>
+  )
+}
